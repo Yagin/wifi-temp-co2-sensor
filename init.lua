af109d04d1cfd30bf5728f9577db96411c2d4eed
Igor Yagin <yagin.igor@gmail.com>
2016-08-21 16:48:53 +0600

    Мелкие исправления
    Основной скрипт

--- init.lua
+++ init.lua
@@ -2,6 +2,8 @@
 -- пока не разобрался почему
 node.setcpufreq(node.CPU160MHZ);
 
+wifi.setmode(wifi.STATIONAP);
+
 -- "подключаем" переменные
 dofile("my_vars.lua");
 -- "подключаем" функции
@@ -9,34 +11,27 @@ dofile("my_funcs.lua");
 -- Подключаемся к AP
 dofile("ap_config.lua");
 
-local vConnCNT=0;
-tmr.alarm(	0,
-			2000,
+-- Основная процедура отправки данных на сервер
+StartSendData();
+
+-- Таймер на случай если нет доступа в интернет
+tmr.alarm(	gIdntTmrConnAP,
+			gAPConnectChkTime,
 			tmr.ALARM_AUTO,
 			function()
-				print("Attemp to connect to the AP #",vConnCNT+1);
-				if wifi.sta.status() ~= 5 then
-					vConnCNT = vConnCNT + 1;
-					-- По истечении 20 секунд, в случае если
-					-- подключение не установлено сообщаем об этом
-					-- и больше не проверяем
-					if vConnCNT > 9 then
-						print("Failed to connect AP")
-						tmr.stop(0)
-					end
-				else
-				    tmr.stop(0)
-				    print("STATION network configuration");
-				    print(wifi.sta.getip());
-				    ProceedData();
+				-- Если доступ есть, останавливаем таймер
+				if gAPConnectAvail then
+					tmr.stop(gIdntTmrConnAP)
 				end
+				-- Пытаемся работать с данными
+				StartSendData()
 			end)
 
 -- Засекам когда пойдут "честные" данные
-tmr.alarm(	1,
+tmr.alarm(	gIdntTmrTrustedData,
 			gDelayBefSending,
 			tmr.ALARM_AUTO,
 			function()
 				gCanSend = true;
-				tmr.stop(1);
+				tmr.stop(gIdntTmrTrustedData);
 			end)
