5550dd79eccef4285bf4ad564f07ba7acf255410
Igor Yagin <yagin.igor@gmail.com>
2016-08-21 16:54:07 +0600

    Различные функции
    Мелкие доработки и исправления

--- my_funcs.lua
+++ my_funcs.lua
@@ -7,6 +7,27 @@ function split(str, sep)
     return result
 end
 
+-- Возвращает конфигурацию сети
+function GetNetworkConf()
+	print();
+	print("=======================================");
+	print("STATION network configuration");
+	vIPv4addr, vNetmask, vGateway = wifi.sta.getip();
+	print("    IPv4 address:",vIPv4addr);
+	print("     Subnet Mask:",vNetmask);
+	print(" Default Gateway:",vGateway);
+	print("Physical Address:",wifi.sta.getmac());
+	print();
+	print("AP network configuration");
+	vIPv4addr, vNetmask, vGateway = wifi.ap.getip();
+	print("    IPv4 address:",vIPv4addr);
+	print("     Subnet Mask:",vNetmask);
+	print(" Default Gateway:",vGateway);
+	print("Physical Address:",wifi.ap.getmac());
+	print("=======================================");
+end -- function GetNetworkConf()
+
+
 -- Отправка данных на сервер GET запросом
 function SendData(iURL, iPPM, iTemp, iHumi)
 	-- Отправляем данные на сервер
@@ -48,12 +69,25 @@ end -- function GetAPList(iTbl)
 
 -- Функция чтения данных с датчиков, и отправка на сервер
 function ProceedData()
-	tmr.alarm(	2,
+	tmr.alarm(	gIdntTmrSensReading,
 				gFreqSensReading,
 				tmr.ALARM_AUTO,
 				function()
+					-- Получаем данные
 					GetData();
-					if gCanSend then
+					-- Проверяем статус сети
+					if wifi.sta.status() ~= 5 then
+						-- Если соединения нет, останавливаем текущий таймер
+						tmr.stop(gIdntTmrSensReading);
+						-- Сообщаем
+						print("Connection lost...")
+						-- Флаг того что соединения нет
+						gAPConnectAvail = false;
+						-- Запускаем таймер проверки наличия
+						-- подключения через 5 минут
+						tmr.start(gIdntTmrConnAP);
+					elseif gCanSend and gAPConnectAvail then
+						-- В случае если соединение есть и данные "честные" - ШЛЁМ
 						SendData(gURL,gPPM,gTemp,gHumi);
 					end
 					collectgarbage();
@@ -61,6 +95,7 @@ function ProceedData()
 end -- function ProceedData()
 
 
+-- Получаем данные с датчика DHT11
 function GetDataDHT11()
 	-- Файл для получения данных с датчика DHT11, записываются в переменную gTemp (Температура) и gHumi (Влажность)
 	Status, Temp, Humi, Temp_dec, Humi_dec = dht.read11(gDHTpin)
@@ -72,3 +107,48 @@ function GetDataDHT11()
 	collectgarbage();
 	return Temp, Humi;
 end -- function GetDataDHT11()
+
+-- Процедура проверки подключения и запуска опроса сенсоров
+function StartSendData()
+
+	local vConnCNT=0;
+
+	tmr.alarm(	gIdntTmrStartSendData,
+			2000,
+			tmr.ALARM_AUTO,
+			function()
+				print("Attemp to connect to the AP #",vConnCNT+1);
+				if wifi.sta.status() ~= 5 then
+					vConnCNT = vConnCNT + 1;
+					-- По истечении 20 секунд, в случае если
+					-- подключение не установлено сообщаем об этом
+					-- и больше не проверяем
+					if vConnCNT > 9 then
+						print("Failed to connect AP");
+						-- Флаг того что соединения нет
+						gAPConnectAvail = false;
+						-- Останавливаем текущий таймер
+						tmr.stop(gIdntTmrStartSendData);
+						-- Опеределяем состояние таймера проверки наличия
+						-- подключения через 5 минут
+						running, mode = tmr.state(gIdntTmrConnAP);
+						-- Запускаем если не работает
+						if running == false then
+							tmr.start(gIdntTmrConnAP);
+						end
+					end
+				else
+					-- Если соединение установлено, останавливаем текущий таймер
+				    tmr.stop(gIdntTmrStartSendData);
+				    -- так же останавливаем таймер проверки наличия
+					-- подключения через 5 минут
+				    tmr.stop(gIdntTmrConnAP);
+				    -- Устанавливаем флаг того что подключение установлено
+				    gAPConnectAvail = true;
+				    -- Выводим конфигурацию сети
+				    GetNetworkConf();
+				    -- Работа с датчиками
+				    ProceedData();
+				end
+			end)
+end -- function StartSendData()
